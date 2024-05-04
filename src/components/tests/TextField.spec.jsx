97e515966eed2ad495e19b891c55fcfd92010b46
cutenam <cutenam@gmail.com>
2024-05-04 16:21:30 +0900

    2.1 단위테스트란 무엇일까 강의 unit-test-example

--- src/components/tests/TextField.spec.jsx
+++ src/components/tests/TextField.spec.jsx
@@ -21,7 +21,8 @@ it('className prop으로 설정한 css class 가 적용된다.', async () => {
   // -> 렌더링 후 DOM에 해당 class가 존재하는지 검증
 
   // 1. Arrange 단계
-  await render(<TextField className="my-class"/>); // render API를 호출 : 테스트를 위한 컴포넌트(TextFiled)를 렌더링
+  await render(<TextField className="my-class"/>);
+  // render API를 호출 : 테스트를 위한 컴포넌트(TextFiled)를 렌더링
   // 테스트 목적인 클래스이름을 props로 전달함 (my-class)
   // render API : 테스트환경의 jsDOM에 리액트 컴포넌트가 렌더링된 DOM구조가 반영
   // jsDOM : Node.js 에서 사용하기 위해 많은 웹표준을 순수 자바스크립트로 구현
@@ -34,7 +35,9 @@ it('className prop으로 설정한 css class 가 적용된다.', async () => {
 
   // 4. 결과 확인
   // 대부분 테스트 라이브러리에서는 expect API를 사용하여 확인가능함. vitest 동일
-  expect(screen.getByPlaceholderText('텍스트를 입력해 주세요.')).toHaveClass('my-class');
+  expect(screen.getByPlaceholderText('텍스트를 입력해 주세요.')).toHaveClass(
+    'my-class'
+  );
   // toHaveClass : Matcher, 렌더링된 요소에 지정된 CSS 클래스가 올바르게 적용되었는지 검증
   // className이란 내부 props나, state 값을 검증(x)
   // 렌더링되는 DOM구조가 올바르게 변경되었는지 확인(o) -> 최종적으로 사용자가 보는 결과는 DOM
